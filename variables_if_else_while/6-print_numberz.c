988c8d53734816f61418691502dc915de1d05275
Marulaska <echetomariana@gmail.com>
2023-02-19 14:12:05 -0800

    numberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
