9553de9ca08a62a416c6f28cc17bf543c20629ab
Marulaska <echetomariana@gmail.com>
2023-02-19 14:17:44 -0800

    reverse alphabet

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 122;
+
+	for (a = 122; a >= 97; a--)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
