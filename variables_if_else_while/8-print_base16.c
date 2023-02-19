6f2cc2e38ecbbbcd8c2299714f281e5bae9e836e
Marulaska <echetomariana@gmail.com>
2023-02-19 14:26:37 -0800

    letters, numbers

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 48;
+
+	for (a = 48; a <= 57; a++)
+	{
+		putchar(a);
+	}
+	for (a = 97; a <= 102; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
