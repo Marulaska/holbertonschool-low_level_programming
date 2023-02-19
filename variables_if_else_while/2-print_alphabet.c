94b892ce7f250558d521f01a264bc93485bc5cea
Marulaska <echetomariana@gmail.com>
2023-02-19 13:16:01 -0800

    putchar

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	for (a = 97; a <= 122; a++)
+	{
+		putchar(a);
+	}
+	putchar(10);
+	return (0);
+}
