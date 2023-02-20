f2fe245e12c7425ed32568517d5f747e207d4b89
Marulaska <echetomariana@gmail.com>
2023-02-19 17:37:07 -0800

    a ver que onda

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - check the code
+ *
+ * Return: Always void
+ */
+void print_alphabet(void)
+{
+	char v;
+
+	for (v = 'a'; v <= 'z'; v++)
+	{
+		_putchar(v);
+	}
+	_putchar('\n');
+}
+
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,1 +1,2 @@
 int _putchar (char v);
+void print_alphabet (void);
