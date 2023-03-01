87716d793c477b3cd0507e7648e8b1b98f857b2f
Marulaska <echetomariana@gmail.com>
2023-03-01 09:07:01 -0800

    keseso

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * reset_to_98 - Updates the value it points to 98
+  * @n: inter
+  *
+  * Return: 98
+  */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
