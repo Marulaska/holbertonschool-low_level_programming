87bb8d5f416f4da65c8d88f9fe61cce5ada9c02b
Marulaska <echetomariana@gmail.com>
2023-03-02 08:18:22 -0800

    swap

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * swap_int - receives two parameters and exchanges the value of one for the other
+ * swap_int - receives two parameters exchanges the value of one for the other
  * @a: first pointer
  * @b: second pointer
  * return: inter
  */
-
-
 void swap_int(int *a, int *b)
 {
 	int n;
