ff12aed24be0f2acac3e8bb21723f39b20df2994
Marulaska <echetomariana@gmail.com>
2023-03-04 15:05:01 -0800

    commit

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strlen - check de length of the code
+ * @s:s char index
+ *
+ * return: length of the code
+ */
+ int _strlen(char *s)
+{
+	int index = 0, sum = 0;
+	char pos = s[index];
+
+	while (pos != '\0') 
+	{
+		sum = sum + 1;
+		pos = s[index++];
+	}
+	return sum;
+}	
+	
