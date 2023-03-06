4fc5588bf2a74b44bc005fbb900f420aaa46de53
Marulaska <echetomariana@gmail.com>
2023-03-05 16:39:54 -0800

    commit

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - check de length of the code
+ * 
+ * @a: int* array of ints
+ * @n: position
+ *
+ * Return: null
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	for (i = 0 ; i < n ; i++)
+	{
+		printf ("%i", a[i]);
+		if (i != n - 1){
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
