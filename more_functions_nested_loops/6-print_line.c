f9dbf995dfc32044b6b735c8ba3494767f0ef6df
Marulaska <echetomariana@gmail.com>
2023-03-09 12:12:24 -0800

    linearecta

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - check the code
+ *
+ * Return: Always void.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = n ; i > 0 ; i--)
+	{
+		_putchar(95);
+
+	}
+	_putchar('\n');
+}
