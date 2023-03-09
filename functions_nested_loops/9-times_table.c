b3802484d3a160486e42c5ed8330aa189c15c069
Marulaska <echetomariana@gmail.com>
2023-03-08 18:33:50 -0800

    times

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,58 @@
+#include "main.h"
+
+/**
+ * putchar_number - auxiliary function
+ * @i: number to be putcharified.
+ *
+ * Return: Always void.
+ */
+void putchar_number(int i)
+{
+	if (i == 0)
+	{
+		_putchar('0');
+	}
+	else if (i / 10 == 0)
+	{
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(i + '0');
+	}
+	else
+	{
+		_putchar(' ');
+		_putchar(i / 10 + '0');
+		_putchar(i % 10 + '0');
+	}
+}
+
+/**
+ * times_table - function that prints times table
+ *
+ * Return: Always void.
+ */
+void times_table(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			/* _putchar (i * j + '0'); */
+			if (i != 0 || (i == 0 && j == 0))
+				putchar_number(i * j);
+			else
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0');
+			}
+			if (j != 9)
+				_putchar(',');
+		}
+
+		_putchar('$');
+		_putchar('\n');
+	}
+}
