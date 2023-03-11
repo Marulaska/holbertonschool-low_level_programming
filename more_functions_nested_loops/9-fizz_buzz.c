d3070e1283705095d6245412700f180fed0a169a
Marulaska <echetomariana@gmail.com>
2023-03-11 14:19:31 -0800

    fizzbuzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -2,33 +2,26 @@
 #include "main.h"
 /**
  * main - check the code
- * @n: input
+ *
  * Return: Always 0.
  */
 int main(void)
 {
 	int n;
-	for (n = 1; n<= 100; n++)
+
+	for (n = 1; n <= 100; n++)
 	{
 		if (n % 3 == 0 && n % 5 == 0)
 			printf("FizzBuzz");
-		
 		else if (n % 3 == 0)
 			printf("Fizz");
-
 		else if (n % 5 == 0)
 			printf("Buzz");
-		
 		else
 			printf("%i", n);
-		printf(" ");
+		if (n != 100)
+			printf(" ");
 	}
 	printf("\n");
-
-	
 	return (0);
 }
-
-
-
-	
