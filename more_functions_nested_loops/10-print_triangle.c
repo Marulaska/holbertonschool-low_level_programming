c74fd8b3ea04ace75846843852b3ceae37cc07ae
Marulaska <echetomariana@gmail.com>
2023-03-11 15:07:46 -0800

    triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_trianlge - print triangle with spaces and hashtag
+ * @size: height of triangle.
+ * Return: Always 0.
+ */
+void print_triangle(int size)
+{
+	int x, y;
+
+	for (y = 1; y <= size; y++)
+	{
+		for (x = size - 1; x >= 0; x--)
+		{
+			if (x >= y)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
+
