bec243b70fc274c5cf6784115f993ca887ddfd24
Marulaska <echetomariana@gmail.com>
2023-03-12 13:18:44 -0700

    capitalizes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * is_sep - Return wether the given char is a separator.
+ * @c: char source
+ *
+ * Return: true if char is separator; false otherwise.
+ */
+int is_sep(char c)
+{
+	int i;
+	char sep[] = " \t\n,;.?\"(){}";
+
+	for (i = 0 ; i < 13; i++)
+	{
+		if (sep[i] == c)
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * cap_string - Return the copy of a given string.
+ * @str: char source
+ *
+ * Return: null
+ */
+char *cap_string(char *str)
+{
+	int index = 0;
+
+	while (str[index] != '\0')
+	{
+		if ((is_sep(str[index]) == 0
+		&& (str[index + 1] >= 'a') && (str[index + 1] <= 'z')))
+			str[index + 1] = str[index + 1] - 32;
+		index++;
+	}
+
+	return (str);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -15,3 +15,4 @@ char *_strcat(char *dest, char *src);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 char *string_toupper(char *str);
+char *cap_string(char *str);
